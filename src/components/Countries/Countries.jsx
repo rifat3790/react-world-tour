36faaa0ab5977abb875db867ddda20f7aa4fdc81
rifat3790 <mdrifayethossen@gmail.com>
2023-09-09 21:59:08 -0700

    react world tour

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,47 @@
+
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Country from '../Country/Country';
+import './Countries.css'
+
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data))
+    },[])
+
+    const handleVisitedCountry = country => {
+        // visitedCountries.push(country);
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    }
+
+
+
+    return (
+        <div>
+            <h3>Countries: {countries.length}</h3>
+            <div>
+                <h5>Visited Countries: {visitedCountries.length}</h5>
+                <ul>
+                    {
+                        visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+                    }
+                </ul>
+            </div>
+            <div className='country-container'>
+            {
+                countries.map(country => <Country country={country} handleVisitedCountry = {handleVisitedCountry} key={country.cca3}></Country>)
+            }
+            </div>
+        </div>
+    );
+};
+
+export default Countries;
